d7baa68ab4db976f86c8ec6e07d89d364be62c2c
katiepeterso <katiepeterso@outlook.com>
2015-09-17 12:21:43 -0700

    add data model files

---
+++ Receipts++/Receipt.m
@@ -0,0 +1,16 @@
+//
+//  Receipt.m
+//  Receipts++
+//
+//  Created by Katherine Peterson on 2015-09-17.
+//  Copyright © 2015 KatieExpatriated. All rights reserved.
+//
+
+#import "Receipt.h"
+#import "Tag.h"
+
+@implementation Receipt
+
+// Insert code here to add functionality to your managed object subclass
+
+@end
---
+++ Receipts++/Tag.m
@@ -0,0 +1,15 @@
+//
+//  Tag.m
+//  Receipts++
+//
+//  Created by Katherine Peterson on 2015-09-17.
+//  Copyright © 2015 KatieExpatriated. All rights reserved.
+//
+
+#import "Tag.h"
+
+@implementation Tag
+
+// Insert code here to add functionality to your managed object subclass
+
+@end
