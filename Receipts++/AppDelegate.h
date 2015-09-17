25ac3384ef9345c228e92cffbc101b7295adfa5c
katiepeterso <katiepeterso@outlook.com>
2015-09-17 12:17:23 -0700

    first commit

---
+++ Receipts++/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Receipts++
+//
+//  Created by Katherine Peterson on 2015-09-17.
+//  Copyright © 2015 KatieExpatriated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Receipts++/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Receipts++
+//
+//  Created by Katherine Peterson on 2015-09-17.
+//  Copyright © 2015 KatieExpatriated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
